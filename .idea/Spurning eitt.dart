9a9a5a3195fe278c31028e70fb42539332aef7a4
Valzionn <144949469+Valzionn@users.noreply.github.com>
2023-10-07 11:31:25 +0000

    ah

--- .idea/Spurning eitt.dart
+++ .idea/Spurning eitt.dart
@@ -1,16 +1,9 @@
+
 void main() {
     int sum = 0;
     for (int i = 1; i <= 10; i++) {
         sum += i;
-        print(i);
+        print('Summa nátturúlegu talnanna (gengur áfram frá síðustu) $i er $sum.');
     }
-    print('Summa talnanna er: $sum.');
-}
-void main() {
-    int sum = 0;
-    for (int i = 1; i <= 10; i++) {
-        sum += i;
-        print('The sum of the first $i natural numbers is $sum.');
-    }
-    print('The sum of the first ten natural numbers is $sum.');
+    print('Summa fyrstu tíu náttúrulegu talnanna er $sum.');
 }
